a5266f21a96597d0256d2360921a0aaa9b065bbd
sc23KS <146721252+sc23KS@users.noreply.github.com>
2023-12-08 17:16:22 +0000

    finished tast 2

--- coursework/tasktwo/StepCounter.c
+++ coursework/tasktwo/StepCounter.c
@@ -7,16 +7,16 @@
 FITNESS_DATA dataArray[10000];
 int counter = 0;
 char buffer[10];
-int choice = 'u';
-char *filename = "FitnessData_2023.csv";
+char choice = 'u';
+char filename[200] = "FitnessData_2023.csv";
 int temp;
 int savedIndex;
 
 int main(){
     /*
     important stuff:
-    - int choice = 'u' scanf(" %i", &choice) if (choice = 'x') all use ascii values
-    --> cast input from ^ scanf from char to ascii
+    - round result in choice=E
+    - check file name input on choice=A
     success criteria:
     - make menu
     - cope with incorrect file name
@@ -33,14 +33,17 @@ int main(){
     //make menu loop until quit is selected
     while(1){
         printf("Menu Options:\nA: Specify the filename to be imported\nB: Display the total number of records in the file\nC: Find the date and time of the timeslot with the fewest steps\nD: Find the date and time of the timeslot with the largest number of steps\nE: Find the mean step count of all the records in the file\nF: Find the longest continuous period where the step count is above 500 steps\nQ: Quit\n");
-        printf("test2\n");
-        scanf(" %i", &choice); //CAST CHOICE VAR TO LOWERCASE
-        printf("%i", choice);
+        scanf(" %c", &choice);
+
+        //casts choice to lowercase
+        if(choice>=65 && choice<=90){
+            choice+=32;
+        }
 
         //gets the new file name, opens the file, reads it, stores the data in an array, closes the file
-        if (choice == 'a'){
+        if (choice == 97){ //a
             printf("Input filename:\n");
-            scanf("%s", filename);
+            scanf(" %s", filename);
             FILE *file = open_file(filename, "r");
 
             int buffer_size = 100;
@@ -59,11 +62,10 @@ int main(){
             }
             fclose(file);
         }
-        else if (choice == 'b'){
-            printf("test");
-            //printf("Total records: %i", counter);
+        else if (choice == 98){ //b
+            printf("Total records: %i\n", counter);
         }
-        else if (choice == 'c'){
+        else if (choice == 99){ //c
             //loop each item in array, from index 0 to counter, linear search for lowest value, print date & time at this index
             temp = 99999;
             for(int i=0;i<counter;i++){
@@ -71,28 +73,28 @@ int main(){
                     temp = dataArray[i].steps;
                     savedIndex = i;
                 }
-                printf("Fewest steps: %s %s", dataArray[savedIndex].date, dataArray[savedIndex].time);
             }
+            printf("Fewest steps: %s %s\n", dataArray[savedIndex].date, dataArray[savedIndex].time);
         }
-        else if (choice == 'd'){
+        else if (choice == 100){ //d
             temp = -1;
             for(int i=0;i<counter;i++){
                 if (dataArray[i].steps>temp){
                     temp = dataArray[i].steps;
                     savedIndex = i;
                 }
-                printf("Largest steps: %s %s", dataArray[savedIndex].date, dataArray[savedIndex].time);
             }
+            printf("Largest steps: %s %s\n", dataArray[savedIndex].date, dataArray[savedIndex].time);
         }
-        else if (choice == 'e'){
+        else if (choice == 101){ //e
             //loop each item in array, index 0 to counter, adding step counts then divide by counter, round as appropriate (DO NOT TRUNCATE)
             temp = 0;
             for(int i=0; i<counter; i++){
                 temp += dataArray[i].steps;
             }
-            printf("KIM ROUND THIS NUMBER\nMean step count: %i", temp/counter);
+            printf("KIM ROUND THIS NUMBER\nMean step count: %g\n", round(temp/counter));
         }
-        else if (choice == 'f'){
+        else if (choice == 102){ //f
             //todo: find the longest period where steps is over 500
             //find streaks of 500+ steps, time taken and index where longest streak starts
             int longestStreak = 0;
@@ -100,7 +102,7 @@ int main(){
             int streakStart = 0;
             int longestStreakStart = 0;
             for(int i=0; i<counter; i++){
-                if (dataArray[i].steps>500){ //>= or just >?
+                if (dataArray[i].steps>500){
                     //increment currentStreak
                     //if currentStreak WAS 0, streakStart = i
                     //continuously increments currentStreak, saving the index the streak starts, until the step count is 500 or less
@@ -119,16 +121,15 @@ int main(){
                     currentStreak = 0;
                 }
             }
-            printf("Longest period start: %s %s", dataArray[longestStreakStart].date, dataArray[longestStreakStart].time);
+            printf("Longest period start: %s %s\n", dataArray[longestStreakStart].date, dataArray[longestStreakStart].time);
             printf("Longest period end: %s %s", dataArray[longestStreakStart+longestStreak].date, dataArray[longestStreakStart+longestStreak].time);
         }
-        else if (choice == 'q'){
+        else if (choice == 113){ //q
             //todo return 0 and quit
             return 0;
             break; //is this necessary? does "return 0" end the program?
         }
         else{
-            //todo: they didn't choose a valid option
             printf("Invalid input, try again\n");
         }
     }
